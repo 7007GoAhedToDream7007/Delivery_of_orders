81fdda482698a4937164a352eae9c435f856e174
RaghadDeveloper <raghadmh620@gmail.com>
2025-01-16 21:09:49 +0300

    store product data and images

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -15,23 +15,23 @@ class StoreSeeder extends Seeder
     {
         Db::table('stores')->insert([
             'store_name' => 'Croma',
-            'store_description' => 'retail shop that sells electrical appliances, gadgets, and equipment, such as refrigerators, washing machines, televisions, and lighting fixtures. It also offers tools, wiring, and other electrical accessories for home and business use.',
-            'store_Image' => 'store1',
+            'store_description' => 'retail shop that sells electrical appliances, gadgets, and equipment, such as refrigerators, washing machines, televisions, and lighting fixtures.',
+            'store_Image' => 'public/images/stores/store1.jpg',
         ]);
         Db::table('stores')->insert([
             'store_name' => 'Coffee Zone',
-            'store_description' => 'Coffee Zone is a popular coffee shop or café chain known for offering a variety of coffee beverages, including espresso-based drinks, iced coffees, and specialty brews. It typically provides a cozy atmosphere for enjoying quality coffee, light snacks, and a place to relax or socialize.',
-            'store_Image' => 'store2',
+            'store_description' => 'Coffee Zone is a popular coffee shop or café chain known for offering a variety of coffee beverages, including espresso-based drinks, iced coffees, and specialty brews.',
+            'store_Image' => 'public/images/stores/store2.jpg',
         ]);
         Db::table('stores')->insert([
             'store_name' => 'Duchess',
             'store_description' => 'It offers a variety of menu options, ranging from casual meals to fine dining, in a setting designed for comfort and service',
-            'store_Image' => 'store3',
+            'store_Image' => 'public/images/stores/store3.jpg',
         ]);
         Db::table('stores')->insert([
             'store_name' => 'Aura',
             'store_description' => ' It offers different styles, sizes, and brands, catering to various fashion preferences and needs.',
-            'store_Image' => 'store4',
+            'store_Image' => 'public/images/stores/store4.jpg',
         ]);
     }
 }
